f5938a06f100a1eeaa48c608da86e65b74e4fc85
Lee Raulin <leeraulin@gmail.com>
2020-02-22 16:26:24 -0500

    wip

--- index.js
+++ index.js
@@ -1,99 +1,164 @@
 "use strict";
 
-// Get table name
-const productDBArn = process.env["Product_DB"]; //'Mark-productTable-1234567';
-const productDBArnArr = productDBArn.split("/");
-const TableName = productDBArnArr[productDBArnArr.length - 1];
-
-// Create connection
+/**
+ * Database connection
+ */
+const TableName = process.env["TABLE_NAME"];
 const AWS = require("aws-sdk");
-
+// TODO: Get region from env
 AWS.config.update({
   region: "us-east-1",
 });
-
-const dynamoDB = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 const documentClient = new AWS.DynamoDB.DocumentClient({
   httpOptions: {
     timeout: 5000,
   },
 });
 
-const createResponse = ({
-  headers = {},
-  body = "",
-  statusCode = 200,
-} = {}) => ({
-  headers,
-  body,
-  statusCode,
+/**
+ * Utility functions
+ */
+const createResponse = responseBody => ({
+  statusCode: 200,
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(responseBody),
+  isBase64Encoded: false,
 });
 
-const getAllProducts = done => {
+/**
+ * CRUD functions
+ */
+const getProductById = (request, done) => {
   const params = {
     TableName,
-    Key: { id: 2 },
+    Key: { id: request.pathParameters.product_id },
   };
 
   documentClient.get(params, (err, data) => {
     if (err) {
-      console.log("Error", err);
+      console.log("getAllProducts--ERROR");
+      console.log(err);
       done(err, null);
     } else {
-      console.log("Success", data.Item);
-      done(null, createResponse({ body: data.Item }));
+      done(null, createResponse(data.Item));
     }
   });
 };
 
+const getProducts = (request, done) => {
+  // const params = {
+  //   TableName,
+  //   KeyConditionExpression: "#yr = :yyyy",
+  //   ExpressionAttributeNames: {
+  //     "#yr": "year",
+  //   },
+  //   ExpressionAttributeValues: {
+  //     ":yyyy": 1985,
+  //   },
+  // };
+  // documentClient.query(params, (err, data) => {
+  //   if (err) {
+  //     console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+  //   } else {
+  //     console.log("Query succeeded.");
+  //     data.Items.forEach(function(item) {
+  //       console.log(" -", item.year + ": " + item.title);
+  //     });
+  //   }
+  // });
+};
+
+const createProduct = (Item, done) => {
+  const params = {
+    TableName,
+    Item,
+    ConditionExpression: "attribute_not_exists(id)",
+  };
+
+  documentClient.put(params, (error, data) => {
+    if (error) {
+      console.log(error);
+      done(error, null);
+    } else {
+      console.log("createProduct: SUCCESS!");
+      done(null, createResponse(data));
+    }
+  });
+};
+
+const bulkCreateProducts = (request, done) => {
+  const products = JSON.parse(request.body);
+
+  const createPutRequest = Item => ({
+    PutRequest: {
+      Item,
+    },
+  });
+
+  const params = {
+    RequestItems: {
+      [TableName]: products.map(p => createPutRequest(p)),
+    },
+  };
+
+  console.log("* * * * bulkCreateProducts: params * * * *");
+  console.log(JSON.stringify(params));
+
+  // const processItemsCallback = function(err, data) {
+  //   if (err) {
+  //     console.log(err);
+  //     done(err, null);
+  //   } else {
+  //     const params = {};
+  //     params.RequestItems = data.UnprocessedItems;
+  //     if (Object.keys(params.RequestItems).length !== 0) {
+  //       db.batchWriteItem(params, processItemsCallback);
+  //     } else {
+  //       console.log(data);
+  //       done(null, createResponse(data));
+  //     }
+  //   }
+  // };
+
+  documentClient.batchWrite(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      done(err, null);
+    } else {
+      console.log(data);
+      done(null, createResponse(data));
+    }
+  });
+};
+
+// GET /products
+const isGetProductsRequest = request =>
+  request.httpMethod === "GET" && request.resource === "/products";
+
+// GET /products/{product_id}
+const isGetProductByIdRequest = request =>
+  request.httpMethod === "GET" && request.resource === "/products/{product_id}";
+
+// POST /products
+const isPostProductsRequest = request =>
+  request.httpMethod === "POST" && request.resource === "/products";
+
 // handleHttpRequest is the entry point for Lambda requests
 exports.handleHttpRequest = (request, context, done) => {
+  console.log("!!! REQUEST !!!");
+  console.log(JSON.stringify(request));
   try {
-    switch (request.httpMethod) {
-      case "GET": {
-        console.log("GET");
-
-        getAllProducts(done);
-
-        break;
-      }
-      case "POST": {
-        console.log("POST");
-        // TODO: check that department is valid before insert
-
-        // There has to be a better way, but...
-        const bodyJSON = JSON.parse(request.body || "{}");
-        for (let key of Object.keys(bodyJSON)) {
-          if (typeof bodyJSON[key] == "number") {
-            bodyJSON[key] = bodyJSON[key].toString();
-          }
-        }
-        const dynamo = new AWS.DynamoDB();
-
-        const params = {
-          TableName,
-          Item: {
-            id: { N: bodyJSON.id },
-            name: { S: bodyJSON.name },
-            description: { S: bodyJSON.description },
-            department: { S: bodyJSON.department },
-            category: { S: bodyJSON.category },
-            photo_url: { S: bodyJSON.photo_url },
-            wholesale_price: { N: bodyJSON.wholesale_price },
-            retail_price: { N: bodyJSON.retail_price },
-            stock: { N: bodyJSON.stock },
-            discountable: { BOOL: bodyJSON.discountable },
-          },
-        };
-        dynamo.putItem(params, (error, data) => {
-          if (error) {
-            console.log(error);
-          } else {
-            done(null, createResponse({ body: data }));
-          }
-        });
-        break;
-      }
+    if (isGetProductsRequest(request)) {
+      console.log("GET /products");
+      getProducts(request, done);
+    }
+    if (isGetProductByIdRequest(request)) {
+      console.log("GET /products/{product_id}");
+      getProductById(request, done);
+    }
+    if (isPostProductsRequest(request)) {
+      console.log("POST /products");
+      bulkCreateProducts(request, done);
     }
   } catch (e) {
     done(e, null);
